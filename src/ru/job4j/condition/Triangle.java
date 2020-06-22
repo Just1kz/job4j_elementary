8038c31cd1a09e927757a16bde909e6a0a5613cd
Just1kz <exelent.pride@rambler.ru>
2020-06-23 01:17:36 +0700

    заготовка, перехожу на другой комп

---
+++ src/ru/job4j/condition/Triangle.java
@@ -0,0 +1,17 @@
+package ru.job4j.condition;
+
+public class Triangle {
+    public static boolean exist(double ab, double ac, double bc) {
+        return false;
+    }
+
+//    double ab, ac, bc - это длины сторон.
+//
+//    Нужно проверить, что ab + ac > bc и ac + bc > ab и ab + bc > ac - в этом случае треугольник существует.
+//
+//    В этом задании нужно использовать булеву логику и оператор && (логическое и).
+public static void main(String[] args) {
+    boolean treg = Triangle.exist(2.)
+}
+
+}
