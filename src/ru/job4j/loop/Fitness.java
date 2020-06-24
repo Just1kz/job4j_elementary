6e13ac84fd6ee67440471160df6deb496d63cb7e
Just1kz <exelent.pride@rambler.ru>
2020-06-24 23:36:58 +0700

    5.4. Протеиновая диета [#306863]

---
+++ src/ru/job4j/loop/Fitness.java
@@ -0,0 +1,19 @@
+package ru.job4j.loop;
+
+public class Fitness {
+    public static int calc(int ivan, int nik) {
+        int month = 0;
+        while (ivan < nik) {
+            ivan *= 3;
+            nik *= 2;
+            month += 1;
+        }
+        return month;
+    }
+
+    public static void main(String[] args) {
+        int x = Fitness.calc(90, 95);
+        System.out.println("Иван обгонит Николая через " + x + " месяцев");
+    }
+
+}
