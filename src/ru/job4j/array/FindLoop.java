90038521752e59459d9858786540c442d38925e2
Just1kz <exelent.pride@rambler.ru>
2020-06-28 02:22:40 +0700

    6.5. Поиск индекса в диапазоне. [#306788]

--- src/ru/job4j/array/FindLoop.java
+++ src/ru/job4j/array/FindLoop.java
@@ -14,7 +14,8 @@ public class FindLoop {
 
     public static int indexOf(int[] data, int el, int start, int finish) {
         int rst = -1; /* если элемента нет в массиве, то возвращаем -1. */
-        for (int index = (start - 1); index <= (finish - 1); index++) {
+        int finishFin = finish >= data.length ? (finish - 1) : finish;
+        for (int index = start; index <= finishFin; index++) {
             if (data[index] == el) {
                 rst = index;
             }
