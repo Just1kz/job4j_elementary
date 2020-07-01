3db936678e6cb41bed1f0a98f5e8b1625fb52bc3
Just1kz <exelent.pride@rambler.ru>
2020-07-02 00:54:44 +0700

    1.5. Поля объекта [#306956]

--- src/ru/job4j/oop/Cat.java
+++ src/ru/job4j/oop/Cat.java
@@ -1,8 +1,35 @@
 package ru.job4j.oop;
 
 public class Cat {
-    public static void main(String[] args) {
+        /* public static void main(String[] args) {
         Cat pappy = new Cat();
-        Cat sparky = new Cat();
-    }
+        Cat sparky = new Cat(); */
+        private String food;
+        private String name;
+
+        public void show() {
+            System.out.println(this.food);
+            System.out.println(this.name);
+        }
+
+        public void eat(String meat) {
+            this.food = meat;
+        }
+
+        public void giveNick(String nick) {
+            this.name = nick;
+        }
+
+        public static void main(String[] args) {
+            System.out.println("There are gav's food.");
+            Cat gav = new Cat();
+            gav.eat("kotleta");
+            gav.giveNick("Milka");
+            gav.show();
+            System.out.println("There are black's food.");
+            Cat black = new Cat();
+            black.eat("fish");
+            black.giveNick("Nigger");
+            black.show();
+        }
 }
