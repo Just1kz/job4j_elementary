a26ad1a4d252fa4b3adbca4288851222b138933e
Just1kz <exelent.pride@rambler.ru>
2020-07-05 21:35:42 +0700

    5. Аннотация @Override [#306969]

--- src/ru/job4j/inheritance/ReportUsage.java
+++ src/ru/job4j/inheritance/ReportUsage.java
@@ -3,8 +3,8 @@ package ru.job4j.inheritance;
 public class ReportUsage {
     public static void main(String[] args) {
         //TextReport report = new TextReport();
-        //HtmlReport report = new HtmlReport();
-        JSONReport report = new JSONReport();
+        HtmlReport report = new HtmlReport();
+        //JSONReport report = new JSONReport();
         String text = report.generate("Anton", "body");
         System.out.println(text);
     }
