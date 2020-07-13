6123e28946a2f0d98e7e41a103311f8ef35499d5
Just1kz <exelent.pride@rambler.ru>
2020-07-13 22:46:33 +0700

    3. Интерфейс Input [#306914]

---
+++ src/ru/job4j/poly/Bus.java
@@ -0,0 +1,19 @@
+package ru.job4j.poly;
+
+public class Bus implements Transport {
+
+    @Override
+    public void go() {
+
+    }
+
+    @Override
+    public String passengers(String passengers) {
+        return null;
+    }
+
+    @Override
+    public String tuck(String truck) {
+        return null;
+    }
+}
---
+++ src/ru/job4j/poly/Transport.java
@@ -0,0 +1,9 @@
+package ru.job4j.poly;
+
+public interface Transport {
+    void go();
+
+    String passengers(String passengers);
+
+    String tuck(String truck);
+}
