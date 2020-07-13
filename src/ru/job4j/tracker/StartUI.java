149b773e74bf61870e912a320f32b389e887c4c6
Just1kz <exelent.pride@rambler.ru>
2020-07-14 02:08:32 +0700

    4.2. Статические методы. [#306919]

--- src/ru/job4j/tracker/StartUI.java
+++ src/ru/job4j/tracker/StartUI.java
@@ -1,72 +1,92 @@
 package ru.job4j.tracker;
 
-
-public class StartUI {
+ public class StartUI {
         public void init(Input input, Tracker tracker) {
             boolean run = true;
             while (run) {
                 this.showMenu();
-                //System.out.print("Select:    ");
-                int select = Integer.valueOf(input.askStr("Select:    "));
+                int select = Integer.parseInt(input.askStr("Select:    "));
                 if (select == 0) {
-                    System.out.println("=== Create a new Item ====");
-                    //System.out.print("Enter name: ");
-                    String name = input.askStr("Enter name: ");
-                    Item item = new Item(name);
-                    tracker.add(item);
+                    StartUI.createItem(input, tracker);
                 } else if (select == 1) {
-                    Item[] showAll = tracker.findAll();
-                    for (int i = 0; i < showAll.length; i++) {
-                        System.out.println(showAll[i]);
-                    }
+                    StartUI.showAll(tracker);
                 } else if (select == 2) {
-                    System.out.println("=== Edit Item ====");
-                    //System.out.print("Enter id, where are you want replace your item: ");
-                    int id = Integer.valueOf(input.askStr("Enter id, where are you want replace your item: "));
-                    //System.out.print("Enter name, what you want replace: ");
-                    String name = input.askStr("Enter name, what you want replace: ");
-                    Item item = new Item(name);
-                    if (tracker.replace(id, item)) {
-                        System.out.println("replaces completed");
-                    } else {
-                        System.out.println("Ошибка при вводе данных, Имя заявки или ID не существует. Требуется повторить операцию с вводом корректных данных");
-                    }
+                    StartUI.editItem(input, tracker);
                 } else if (select == 3) {
-                    System.out.println("=== Delete Item ====");
-                    //System.out.print("Enter id: ");
-                    int id = Integer.valueOf(input.askStr("Enter id: "));
-                    if (tracker.delete(id)) {
-                        System.out.println("delete completed");
-                    } else {
-                        System.out.println("Ошибка при вводе данных, ID не существует. Требуется повторить операцию с вводом корректных данных");
-                    }
+                    StartUI.deleteItem(input, tracker);
                 } else if (select == 4) {
-                    System.out.println("=== Find by Id ====");
-                    //System.out.print("Enter id: ");
-                    int id = Integer.valueOf(input.askStr("Enter id: "));
-                    Item item = tracker.findById(id);
-                        if (item != null) {
-                            System.out.println(item);
-                        } else {
-                            System.out.println("Ошибка при вводе данных, ID не существует. Требуется повторить операцию с вводом корректных данных");
-                        }
+                    StartUI.findByID(input, tracker);
                 } else if (select == 5) {
-                    System.out.println("=== Find by Name ====");
-                    //System.out.print("Enter name to search: ");
-                    String name = input.askStr("Enter name to search: ");
-                    Item[] showAllNameSearch = tracker.findByName(name);
-                    if (showAllNameSearch.length > 0) {
-                        for (int i = 0; i < showAllNameSearch.length; i++) {
-                            System.out.println(showAllNameSearch[i]);
-                        }
-                    } else {
-                        System.out.println("Ошибка при вводе данных, Имя заявки не существует. Требуется повторить операцию с вводом корректных данных");
-                    }
+                    StartUI.findbyName(input, tracker);
                 } else if (select == 6) {
                     run = false;
                 }
             }
         }
+
+     public static void findbyName(Input input, Tracker tracker) {
+         System.out.println("=== Find by Name ====");
+         String name = input.askStr("Enter name to search: ");
+         Item[] showAllNameSearch = tracker.findByName(name);
+         if (showAllNameSearch.length > 0) {
+             for (int i = 0; i < showAllNameSearch.length; i++) {
+                 System.out.println(showAllNameSearch[i]);
+             }
+         } else {
+             System.out.println("Ошибка при вводе данных, Имя заявки не существует. Требуется повторить операцию с вводом корректных данных");
+         }
+     }
+
+     public static void findByID(Input input, Tracker tracker) {
+         System.out.println("=== Find by Id ====");
+         int id = Integer.parseInt(input.askStr("Enter id: "));
+         Item item = tracker.findById(id);
+         if (item != null) {
+             System.out.println(item);
+         } else {
+             System.out.println("Ошибка при вводе данных, ID не существует. Требуется повторить операцию с вводом корректных данных");
+         }
+     }
+
+     public static void showAll(Tracker tracker) {
+         Item[] showAll = tracker.findAll();
+         if (showAll.length > 0) {
+             for (int i = 0; i < showAll.length; i++) {
+                 System.out.println(showAll[i]);
+             }
+         } else {
+             System.out.println("Store is empty.");
+         }
+     }
+
+     public static void deleteItem(Input input, Tracker tracker) {
+         System.out.println("=== Delete Item ====");
+         int id = Integer.parseInt(input.askStr("Enter id: "));
+         if (tracker.delete(id)) {
+             System.out.println("delete completed");
+         } else {
+             System.out.println("Ошибка при вводе данных, ID не существует. Требуется повторить операцию с вводом корректных данных");
+         }
+     }
+
+     public static void editItem(Input input, Tracker tracker) {
+         System.out.println("=== Edit Item ====");
+         int id = Integer.parseInt(input.askStr("Enter id, where are you want replace your item: "));
+         String name = input.askStr("Enter name, what you want replace: ");
+         Item item = new Item(name);
+         if (tracker.replace(id, item)) {
+             System.out.println("replaces completed");
+         } else {
+             System.out.println("Ошибка при вводе данных, Имя заявки или ID не существует. Требуется повторить операцию с вводом корректных данных");
+         }
+     }
+
+     public static void createItem(Input input, Tracker tracker) {
+         System.out.println("=== Create a new Item ====");
+         String name = input.askStr("Enter name: ");
+         Item item = new Item(name);
+         tracker.add(item);
+    }
 
         private void showMenu() {
             System.out.println("Menu:");
@@ -79,10 +99,10 @@ public class StartUI {
             System.out.println("6. Exit Program");
         }
 
-
         public static void main(String[] args) {
             Input input = new ConsoleInput(System.in);
             Tracker tracker = new Tracker();
             new StartUI().init(input, tracker);
+            StartUI.createItem(input, tracker);
         }
 }
