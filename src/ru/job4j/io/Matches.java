edf590f271854f95ba5ef4bf460e7a39fde0c13a
Just1kz <exelent.pride@rambler.ru>
2020-07-13 17:04:18 +0700

    2. Scanner и чтение числа из консоли. [#306913]

--- src/ru/job4j/io/Matches.java
+++ src/ru/job4j/io/Matches.java
@@ -26,23 +26,6 @@ public class Matches {
                 break;
             }
             gamer = !gamer;
-            name = gamer ? "игрок1" : "игрок2";
-            System.out.println(name + " тяните спички (от 1 до 3-х)   ");
-            int player2Step = Integer.valueOf(input.nextLine());
-            while (winScore > 0) {
-                if ((player2Step == 1) || (player2Step == 2) || (player2Step == 3)) {
-                    winScore = winScore - player2Step;
-                    break;
-                } else {
-                    System.out.println("Вы ввели недопустимое число спичек, требуется от 1 до 3 выбрать значение, просьба повторить ввод данных");
-                    player2Step = Integer.valueOf(input.nextLine());
-                }
-            }
-            if (winScore < 0) {
-                System.out.println("Game over, Winner " + name);
-                break;
-            }
-            gamer = !gamer;
         }
     }
 }
