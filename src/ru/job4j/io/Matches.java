72d1815ee32a7fa588009a529f37ec661b2e3e9d
Just1kz <exelent.pride@rambler.ru>
2020-07-13 16:55:25 +0700

    2. Scanner и чтение числа из консоли. [#306913]

--- src/ru/job4j/io/Matches.java
+++ src/ru/job4j/io/Matches.java
@@ -5,30 +5,44 @@ import java.util.Scanner;
 public class Matches {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-        boolean gameover = true;
-        int nowScore = 0;
+        boolean gamer = true;
         int winScore = 11;
-        int result;
         System.out.println("Игра 11, началась:");
-        while (gameover) {
-            result = winScore - nowScore;
-            System.out.println("Спичек осталось на столе: " + result);
-            System.out.println("Игрок №1 тяните спички (от 1 до 3-х)   ");
+        while (winScore > 0) {
+            String name = gamer ? "игрок1" : "игрок2";
+            System.out.println(name + " тяните спички (от 1 до 3-х)   ");
             int player1Step = Integer.valueOf(input.nextLine());
-            nowScore += player1Step;
-            if (nowScore >= winScore) {
-                gameover = false;
-                System.out.println("Game over, Winner player1");
+                while (winScore > 0) {
+                    if ((player1Step == 1) || (player1Step == 2) || (player1Step == 3)) {
+                        winScore = winScore - player1Step;
+                        break;
+                    } else {
+                        System.out.println("Вы ввели недопустимое число спичек, требуется от 1 до 3 выбрать значение, просьба повторить ввод данных");
+                        player1Step = Integer.valueOf(input.nextLine());
+                    }
+                }
+            if (winScore < 0) {
+                System.out.println("Game over, Winner " + name);
+                break;
             }
-            result = winScore - nowScore;
-            System.out.println("Спичек осталось на столе: " + result);
-            System.out.println("Игрок №2 тяните спички (от 1 до 3-х)   ");
+            gamer = !gamer;
+            name = gamer ? "игрок1" : "игрок2";
+            System.out.println(name + " тяните спички (от 1 до 3-х)   ");
             int player2Step = Integer.valueOf(input.nextLine());
-            nowScore += player2Step;
-            if (nowScore >= winScore) {
-                gameover = false;
-                System.out.println("Game is over, Winner player2");
+            while (winScore > 0) {
+                if ((player2Step == 1) || (player2Step == 2) || (player2Step == 3)) {
+                    winScore = winScore - player2Step;
+                    break;
+                } else {
+                    System.out.println("Вы ввели недопустимое число спичек, требуется от 1 до 3 выбрать значение, просьба повторить ввод данных");
+                    player2Step = Integer.valueOf(input.nextLine());
+                }
             }
+            if (winScore < 0) {
+                System.out.println("Game over, Winner " + name);
+                break;
+            }
+            gamer = !gamer;
         }
     }
 }
