ec317074c5d09644ce0de7ae6b89e98369f11a89
Just1kz <exelent.pride@rambler.ru>
2020-07-15 23:26:05 +0700

    0.2. Кидаем исключение - throw new RuntimeException [#306931]

---
+++ src/ru/job4j/ex/Fact.java
@@ -0,0 +1,20 @@
+package ru.job4j.ex;
+
+public class Fact {
+    public static void main(String[] args) {
+        Fact fact = new Fact();
+        int a = fact.calc(-2);
+        System.out.println(a);
+    }
+
+    public int calc(int n) {
+        if (n < 0) {
+            throw new IllegalArgumentException("Entered data is incorrect");
+        }
+        int rsl = 1;
+        for (int index = 1; index <= n; index++) {
+            rsl *= index;
+        }
+        return rsl;
+    }
+}
